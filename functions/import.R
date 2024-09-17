ecc21c2f302fb1055bb2c6b73e0d1a81d361560f
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2024-09-17 13:39:03 +0200

    Create import.R

---
+++ functions/import.R
@@ -0,0 +1,23 @@
+import <- function(...) {
+  #' Import R packages. Install them if necessary.
+  #' 
+  #' @param ... any argument that can be passed to install.packages.
+  #' @details The function installs only packages that are missing. Packages
+  #' are loaded.
+  #' @examples
+  #' # Load packages
+  #' import("dplyr", "MASS", "terra", dependencies = TRUE)
+  #' 
+  #' @seealso \code{\link[base]{install.packages}}
+  #' @export
+  args <- list(...)
+  packages = args[names(args) == ""]
+  kwargs = args[names(args) != ""]
+  
+  for (package in packages) {
+    if (!require(package, character.only = TRUE)) {
+      do.call(install.packages, package, kwargs)
+    }
+    require(package, character.only = TRUE)
+  }
+}
